a215bd5bc23bf3eea0ffca3273ee7f8be086f14f
Kevin Folk <folkkevi@gmail.com>
2013-03-13 23:00:21 -0500

    New homepage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,20 +1,110 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Upload Form</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Chronocolor</title>
+
+<script type="text/javascript">
+//change this to true to enable troubleshooting mode
+var myfont_webfont_test = false;
+// change this to false if you're having trouble with WOFFs
+var woffEnabled = true;
+// to place your webfonts in a custom directory, uncomment this and set it to where your webfonts are.
+var customPath = "/fonts";
+</script>
+<script type="text/javascript" src="./fonts/Webfonts.js"></script>
+
+<link rel="stylesheet" type="text/css" href="style/style.css">
+
 </head>
+
 <body>
 
-<?php if(isset($error)) echo $error;?>
+<div id="container">
 
-<?php echo form_open_multipart('image/analyze');?>
+	<div class="header">
+    	<div class="contentblock">
+    		<h1>Chrono<span id="color">Color</span></h1>
+        	<p class="blocktext">ChronoColor is a joint project between Kevin Folk and Thomas Jacobs. The intent is to display the breadth of an artist's work in an interactive graphical format. A comprehensive selection of the artists' work is displayed so that conclusions may be drawn about their process, tendencies, and development on a macro level. Enjoy.</p>
+        </div>
+    </div>
+    
+    <div class="divider"></div>
+    
+    <!-- Start main body here -->
+    
+    <div class="contentblock">
+    
+    	<div>
+        	<h2>Pablo Piccaso</h2>
+            <p class="date">1881&ndash;1973</p>
+        </div>
+        <div class="divider_small"></div>
+        
+        <div class="timeline">
+        	<div class="period">
+            	<div class="vdivider"></div>
+                <div class="timeperiod">
+                	<h3>Early Years</h3>
+                	<p class="date">1889&ndash;1900</p>
+                </div>
+                <span class="clear"></span>
+            </div>
+        	<div class="colorbar">
+            </div>
+        </div>
+        
+        <div class="detailblock">
+        	<div class="detailcontainer">
+        		<div class="arrow">
+           			<img src="img/arrowl.png" width="52" height="44" alt="" />
+           		</div>
+        	
+            	<div class="detail">
+            
+        			<div class="detailbox">
+                
+                		<div class="workimg">
+                    		<img src="img/workimg.jpg" width="248" height="300" alt="" />
+                    	</div>
+                    
+                    	<div class="workcolors">
+                    		<div class="workcolor" id="workcolor1"></div>
+                        	<div class="workcolor" id="workcolor2"></div>
+                        	<div class="workcolor" id="workcolor3"></div>
+                        	<div class="workcolor" id="workcolor4"></div>
+                    	</div>
+                    
+        			</div>
+                
+                <div class="detailtext">
+                	<h3 class="worktitle">Portrait de Sebastià Junyent</h3>
+                    <p class="workdate">1904</p>
+					<p class="workdescription">Oil on canvas, 73 x 60 cm</p>
+                </div>
+                
+            </div>
+            
+            <div class="arrow">
+            <img src="img/arrowr.png" width="52" height="44" alt="" />
+            </div>
+            
+            </div>
 
-<input type="file" name="userfile" size="20" />
-
-<br /><br />
-
-<input type="submit" value="upload" />
-
-</form>
+      	</div>
+		<span class="clear"></span>
+    </div>
+   
+    <!-- End main body-->
+    
+    <div class="divider"></div>
+     
+    <div class="header">
+    	<div class="contentblock">Footer
+        </div>
+    </div>
+    
+</div>
 
 </body>
 </html>
